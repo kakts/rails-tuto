b650f1f7cca2a86b48b0af3112e378791f3c5602
Keita Akutsu <kakts.git@gmail.com>
2020-04-11 01:24:29 +0900

    1.3.4 Hello World! set ApplicationController#hello as root.

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello world!"
+  end
+
+  def hello2
+    render html: "hello world 2!"
+  end
 end
--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # アプリケーションのルートを ApplicationController#helloに割り当てる
+  root 'application#hello'
 end
