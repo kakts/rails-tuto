8cb200587246d7f9a5f65350a044c8521313a344
Keita Akutsu <kakts.git@gmail.com>
2020-04-13 00:58:37 +0900

    2.2.2 Refactor: add comment in config/routes.rb

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # :usersという記法はRuby言語特有の「シンボル」と呼ばれるもの詳細は4.3.3で
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
