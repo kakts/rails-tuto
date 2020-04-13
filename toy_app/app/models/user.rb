6bd802bb38899991800f7423c5e2106a8f7439e2
Keita Akutsu <kakts.git@gmail.com>
2020-04-14 00:07:33 +0900

    2.3.3 q-3 add validation to User.name and User.email

--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   # UserとMicropostsの関係 1UserがMicropostsを複数持ち得る
   has_many :microposts
+
+  # バリデーション
+  validates :name, presence: true
+  validates :email, presence: true
 end
