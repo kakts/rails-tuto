7b803b8ad17f48f903ea086672bad6373d3d8bbe
Keita Akutsu <kakts.git@gmail.com>
2020-04-13 23:57:33 +0900

    add has_many and belongs_to between User and Microposts

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
+  # MicropostがUserモデルに従属していることの指定
+  belongs_to :user
+
+  # バリデーション
   validates :content, length: { maximum: 140 }
 end
--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  # UserとMicropostsの関係 1UserがMicropostsを複数持ち得る
+  has_many :microposts
 end
